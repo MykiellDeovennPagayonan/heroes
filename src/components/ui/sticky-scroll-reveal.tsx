6f17b379ac6e2fb54becd17312b9bce1483cadb4
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-06-24 16:24:24 +0800

    [Add] How to be a hero

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -67,7 +67,7 @@ export const StickyScroll = ({
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="w-full h-full bg-dot-white/[0.15] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
+      className="w-full h-full bg-dot-white/[0.15] overflow-y-auto no-scrollbar flex justify-center relative space-x-10 rounded-md p-10"
       ref={ref}
     >
       <div className="div relative flex items-start px-4">
